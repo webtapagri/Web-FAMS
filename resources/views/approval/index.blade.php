a45316ceb64a049f404714039045cc7df4727433
analuthfa <luthfa.ana@gmail.com>
2020-05-29 10:06:41 +0700

    reset approval.index

--- resources/views/approval/index.blade.php
+++ resources/views/approval/index.blade.php
@@ -791,7 +791,7 @@
                     },
                     {
                         data: 'REQUEST_DATE',
-                        name: 'REQUEST_DATE',
+                        name: 'REQUEST_DATE'
                     },
                     {
                         data: 'REQUESTOR',
@@ -865,7 +865,6 @@
                     sLoadingRecords: ""
                 },
                 "order": [],
-                // "order": [[4, 'desc']],
             },
         });
 
