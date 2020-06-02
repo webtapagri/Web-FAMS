0ad99dc6db44b6f6d952224c4a976c09ce011525
analuthfa <luthfa.ana@gmail.com>
2020-06-02 17:36:42 +0700

    concat

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -119,7 +119,7 @@ class ReportController extends Controller
         }
 
         
-        DB::unprepared("SET SESSION group_concat_max_len = 1000000;");
+        DB::unprepared("SET SESSION group_concat_max_len = 40000000;");
         $sql = " SELECT a.*, b.DESCRIPTION AS NAMA_PT_PEMILIK,e.NAMA_VENDOR, c.FOTO_ASET,c.FOTO_SERI,c.FOTO_MESIN,
                         f.JENIS_ASSET_DESCRIPTION as JENIS_ASSET_NAME,
                         g.GROUP_DESCRIPTION as GROUP_NAME, 
