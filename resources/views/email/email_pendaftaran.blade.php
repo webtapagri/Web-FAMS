7258e70a3883ef5e1afebb114f9363ae2948ddf8
andipahlevy <andilevi@gmail.com>
2020-07-30 11:07:02 +0700

    fix detail url

--- resources/views/email/email_pendaftaran.blade.php
+++ resources/views/email/email_pendaftaran.blade.php
@@ -41,7 +41,7 @@ Dibutuhkan persetujuan atas dokumen berikut :
 <br/>
 <br/>
 <div style='margin-left:10%'>
-	NO DOCUMENT : {{ $data->datax[0]->document_code }} (<a href="<?php echo url('/?noreg='.$dc.'') ?>" target="_blank">detail</a>) <br/>
+	NO DOCUMENT : {{ $data->datax[0]->document_code }} (<a href="<?php echo $data->detail_url ?>" target="_blank">detail</a>) <br/>
 	TYPE OF SUBMISSION : {{ $TYPE_OF_SUBMISSION }} <br/>
 	PURCHASE NO : {{ $data->datax[0]->NO_PO }} <br/>
 	LOKASI KEPEMILIKAN ASET : {{ $data->datax[0]->BA_PEMILIK_ASSET }} - {{ $data->datax[0]->BA_PEMILIK_ASSET_DESC }} <br/><br/>
