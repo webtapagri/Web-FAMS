151abe467b316dad8d19d496d8cd8e2ce0dbf1f8
analuthfa <luthfa.ana@gmail.com>
2020-08-11 16:09:42 +0700

    approve email

--- app/Http/Controllers/ApprovalController.php
+++ app/Http/Controllers/ApprovalController.php
@@ -3016,7 +3016,7 @@ WHERE a.no_reg = '".$noreg."' AND b.MANDATORY_KODE_ASSET_CONTROLLER = 'X' ORDER 
            
         }
     }
-    function update_status_disposal_email()
+    function update_status_disposal_email(Request $request)
     {
         $request = new \Illuminate\Http\Request();
 
