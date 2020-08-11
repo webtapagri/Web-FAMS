d3cc0ce0b287e39e38eb6f1ed5f98f31d209a1d1
analuthfa <luthfa.ana@gmail.com>
2020-08-11 14:34:34 +0700

    reject note

--- app/Http/Controllers/FamsEmailController.php
+++ app/Http/Controllers/FamsEmailController.php
@@ -164,9 +164,10 @@ class FamsEmailController extends Controller
 	{		
 		$request = new \Illuminate\Http\Request();
 		$request->replace(['id' => $_GET['id']]);
-		$req['message'] = unserialize(urldecode($_GET['id']));
+		$message = unserialize(urldecode($_GET['id']));
 		// return Redirect::route('mail_response', array('message' => $req));		
-        return view('email.respon')->with(compact('req'));
+		// return view('email.respon')->with(compact('req'));
+		return view('greetings')->with('message', $message);
 
 		// $request->replace(['id' => $_GET['id']]);
 
