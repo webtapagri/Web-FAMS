4a73278d89aa97addd9c6c036edffd7b1c13895e
analuthfa <luthfa.ana@gmail.com>
2020-08-11 16:13:56 +0700

    approve email

--- resources/views/email/respon.blade.php
+++ resources/views/email/respon.blade.php
@@ -65,7 +65,7 @@ function changeStatusDisposal(status)
             });
             
             $.ajax({
-                url: "{{ url('approval/update_status_disposal_email') }}?"+param,
+                url: "{{ url('approval/update_status_disposal_email') }}?id="+param,
                 method: "POST",
                 data: param ,
                 success: function() 
