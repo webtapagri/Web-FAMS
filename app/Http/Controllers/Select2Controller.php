2e0c1707f3ed990efa58a74c45af94ab973e3576
analuthfa <luthfa.ana@gmail.com>
2021-06-02 08:23:28 +0700

    order jenis asset

--- app/Http/Controllers/Select2Controller.php
+++ app/Http/Controllers/Select2Controller.php
@@ -133,7 +133,8 @@ class Select2Controller extends Controller
         WHERE B.DESCRIPTION_CODE = '$request->ba_code' AND B.DESCRIPTION = SUBSTR( A.JENIS_ASSET_CODE, 1, 1 )
         UNION
         SELECT A.JENIS_ASSET_CODE as id, A.JENIS_ASSET_DESCRIPTION as text FROM TM_JENIS_ASSET A
-        WHERE SUBSTR( A.JENIS_ASSET_CODE, 1, 1 ) = 'U' ";
+        WHERE SUBSTR( A.JENIS_ASSET_CODE, 1, 1 ) = 'U'
+        order by id asc ";
         $data = DB::select(DB::raw($sql));
         $arr = array();
         $arr[] = array("id"=>"","text"=>"");
