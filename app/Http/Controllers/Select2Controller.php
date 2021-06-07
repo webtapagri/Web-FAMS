4d6c4bf9f81d353694dd900c0bfd6b2c81702fc0
analuthfa <luthfa.ana@gmail.com>
2021-06-07 14:10:42 +0700

    select2 order

--- app/Http/Controllers/Select2Controller.php
+++ app/Http/Controllers/Select2Controller.php
@@ -137,9 +137,7 @@ class Select2Controller extends Controller
                 SELECT A.JENIS_ASSET_CODE as id, A.JENIS_ASSET_DESCRIPTION as text FROM TM_JENIS_ASSET A
                 WHERE SUBSTR( A.JENIS_ASSET_CODE, 1, 1 ) = 'U') A
                 order by A.id asc";
-        $data = DB::select(DB::raw($sql))
-        ->orderby('id', 'asc')
-        ->get();
+        $data = DB::select(DB::raw($sql));
         $arr = array();
         $arr[] = array("id"=>"","text"=>"");
         foreach ($data as $row) 
